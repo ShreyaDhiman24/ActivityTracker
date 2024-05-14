726e6fcefdd93760e4339e888d8742f330994e56
Shreya Dhiman <shreyanksha2004@gmail.com>
2024-05-14 17:34:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LoginRegister from './LoginRegister/LoginRegister';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='heading'>Activity Tracker</h1>
+      {/* <LoginRegister/> */}
+      
+    </div>
+  );
+}
+
+export default App;
